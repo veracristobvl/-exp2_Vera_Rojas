62e5b0f4c9d97f126c651b1d2ecdcd6f3259f39e
cristobal vera <cr.verar@duocuc.cl>
2023-05-08 22:30:36 -0400

    Entrega 2

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,17 +1,1 @@
-[
-    {
-        "Id": 1,
-        "title": "VILMORIN",
-        "descripcion": "Semillas de Reina Luisa Enana",
-        "precio": 1.990,
-        "imgURL" : "img/img-grid-1.webp"
-    },
-    {
-        "Id": 2,
-        "title": "FISKARS",
-        "descripcion": "Pala Manual para Jardín 200S",
-        "precio": 5.990,
-        "imgURL" : "img/img-grid-5.webp"
-      }
-]
 
